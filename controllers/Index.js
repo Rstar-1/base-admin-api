23209b5e5c237c20dd242b20f4ca555b2e51c998
Rstar-1 <rajshetye.5855@gmail.com>
2024-06-23 15:50:04 +0530

    gf

--- controllers/Index.js
+++ controllers/Index.js
@@ -1,9 +1,15 @@
 const SeoController = require('../controllers/seocontroller/SeoController.js');
 const CmsTextController = require('../controllers/cmscontroller/CmsTextController.js');
 const CmsImageController = require("../controllers/cmscontroller/CmsImageController.js");
+const DashboardController = require("../controllers/automationcontroller/DashboardController.js");
+const ManagementController = require("../controllers/automationcontroller/ManagementController.js");
+const SidebarController = require("../controllers/automationcontroller/SidebarController.js");
 
 module.exports = {
   SeoController,
   CmsTextController,
   CmsImageController,
+  DashboardController,
+  ManagementController,
+  SidebarController,
 };
---
+++ routes/automationroutes/ManagementRoutes.js
@@ -0,0 +1,33 @@
+const Controller = require("../../controllers");
+const path = require("path");
+const multer = require("multer");
+const { fileUpload } = require("../../middleware/FileUpload");
+const { uploadToCloud } = require("../../middleware/Upload");
+
+module.exports = function (app) {
+  app.post(
+    "/managementadddata",
+    fileUpload("picture", false),
+    uploadToCloud(),
+    Controller.ManagementController.managementadddata
+  );
+  app.get("/managementgetdata", Controller.ManagementController.managementgetdata);
+  app.post(
+    "/managementpaginationdata",
+    Controller.ManagementController.managementpaginationdata
+  );
+  app.get(
+    "/managementsingledata/:id",
+    Controller.ManagementController.managementsingledata
+  );
+  app.patch(
+    "/managementupdatedata/:id",
+    fileUpload("picture", false),
+    uploadToCloud(),
+    Controller.ManagementController.managementupdatedata
+  );
+  app.delete(
+    "/managementdeletedata/:id",
+    Controller.ManagementController.managementdeletedata
+  );
+};
